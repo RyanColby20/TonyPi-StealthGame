7492a795aa023a53d831b6aaea9f03ab46bd22b1
beansprout214 <brightest21@outlook.com>
2026-04-09 23:02:43 -0400

    gamepadtest file is using dummy robot

---
+++ Functions/Gamepad/GamepadTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# coding=utf8
+
+from Gamepad import Gamepad
+from ...robot_functions.robot_controller_dummy import TonyPiController   # whatever class wraps run_action(), stop_actions(), etc.
+
+def main():
+    robot = TonyPiController()              # your robot control class
+    pad = Gamepad(robot)         # create gamepad controller
+    pad.start()                  # start background thread
+
+    print("Gamepad control running. Press Ctrl+C to exit.")
+
+    try:
+        while True:
+            pass                 # keep main thread alive
+    except KeyboardInterrupt:
+        print("Stopping...")
+        pad.stop()
+
+if __name__ == "__main__":
+    main()
