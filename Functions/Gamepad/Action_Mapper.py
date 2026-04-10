e1e8e11dfda79bc7b6fb6b4928d33efbf7641c19
beansprout214 <brightest21@outlook.com>
2026-04-09 22:41:38 -0400

    created gamepad.py and dependents. split apart for easier debugging/modifying. testing is tbd

---
+++ Functions/Gamepad/Action_Mapper.py
@@ -0,0 +1,29 @@
+import time
+
+from .Constants import *
+
+class ActionMapper:
+    """
+    Maps controller buttons to TonyPi action groups.
+    """
+    def __init__(self, robot, learn_mode=False):
+        self.robot = robot
+        self.learn_mode = learn_mode
+        self.last_fire = {}
+
+        # Map buttons → action names
+        self.map = {
+            self.BTN_SQUARE: "wave",
+            self.BTN_CROSS: "bow",
+            self.BTN_TRIANGLE: "twist",
+            self.BTN_CIRCLE: "crouch",
+        }
+
+    # ---------------------------------------------------------
+    def handle_button(self, b):
+        now = time.time()
+        if b in self.map:
+            if (now - self.last_fire.get(b, 0)) >= self.BUTTON_COOLDOWN:
+                self.last_fire[b] = now
+                action = self.map[b]
+                self.robot.run_action(action, times=1)
